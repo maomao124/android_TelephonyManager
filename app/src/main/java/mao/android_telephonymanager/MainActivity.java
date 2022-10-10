0466d0ca88865f22132289685bac19b9e119ca29
mao <1296193245@qq.com>
2022-10-10 13:48:26 +0800

    android_TelephonyManager

---
+++ app/src/main/java/mao/android_telephonymanager/MainActivity.java
@@ -0,0 +1,199 @@
+package mao.android_telephonymanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private TextView tv_phone1;
+    private TextView tv_phone2;
+    private TextView tv_phone3;
+    private TextView tv_phone4;
+    private TextView tv_phone5;
+    private TextView tv_phone6;
+    private TextView tv_phone7;
+    private TextView tv_phone8;
+    private TextView tv_phone9;
+    private TelephonyManager telephonyManager;
+    private final String[] phoneType = {"未知", "2G", "3G", "4G"};
+    private final String[] simState = {"状态未知", "无SIM卡", "被PIN加锁", "被PUK加锁",
+            "被NetWork PIN加锁", "已准备好"};
+
+    private final String[] permission =
+            {
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.READ_PHONE_STATE,
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        bindViews(168);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void bindViews()
+    {
+        tv_phone1 = (TextView) findViewById(R.id.tv_phone1);
+        tv_phone2 = (TextView) findViewById(R.id.tv_phone2);
+        tv_phone3 = (TextView) findViewById(R.id.tv_phone3);
+        tv_phone4 = (TextView) findViewById(R.id.tv_phone4);
+        tv_phone5 = (TextView) findViewById(R.id.tv_phone5);
+        tv_phone6 = (TextView) findViewById(R.id.tv_phone6);
+        tv_phone7 = (TextView) findViewById(R.id.tv_phone7);
+        tv_phone8 = (TextView) findViewById(R.id.tv_phone8);
+        tv_phone9 = (TextView) findViewById(R.id.tv_phone9);
+
+        tv_phone1.setText("设备编号：" + telephonyManager.getDeviceId());
+        tv_phone2.setText("软件版本：" + (telephonyManager.getDeviceSoftwareVersion() != null ?
+                telephonyManager.getDeviceSoftwareVersion() : "未知"));
+        tv_phone3.setText("运营商代号：" + telephonyManager.getNetworkOperator());
+        tv_phone4.setText("运营商名称：" + telephonyManager.getNetworkOperatorName());
+        tv_phone5.setText("网络类型：" + phoneType[telephonyManager.getPhoneType()]);
+        tv_phone6.setText("设备当前位置：" + (telephonyManager.getCellLocation() != null ? telephonyManager
+                .getCellLocation().toString() : "未知位置"));
+        tv_phone7.setText("SIM卡的国别：" + telephonyManager.getSimCountryIso());
+        tv_phone8.setText("SIM卡序列号：" + telephonyManager.getSimSerialNumber());
+        tv_phone9.setText("SIM卡状态：" + simState[telephonyManager.getSimState()]);
+    }
+
+
+    public void bindViews(int requestCode)
+    {
+        if (checkPermission(MainActivity.this, permission,
+                requestCode % 65536))
+        {
+            bindViews();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // requestCode不能为负数，也不能大于2的16次方即65536
+        if (requestCode == 168 % 65536)
+        {
+            if (checkGrant(grantResults))
+            {
+                bindViews();
+            }
+            else
+            {
+                toastShow("没有权限");
+            }
+        }
+    }
+
+
+    /**
+     * 检查权限结果数组，
+     *
+     * @param grantResults 授予相应权限的结果是PackageManager.PERMISSION_GRANTED
+     *                     或PackageManager.PERMISSION_DENIED
+     *                     从不为空
+     * @return boolean 返回true表示都已经获得授权 返回false表示至少有一个未获得授权
+     */
+    public static boolean checkGrant(int[] grantResults)
+    {
+        boolean result = true;
+        if (grantResults != null)
+        {
+            for (int grant : grantResults)
+            {
+                //遍历权限结果数组中的每条选择结果
+                if (grant != PackageManager.PERMISSION_GRANTED)
+                {
+                    //未获得授权，返回false
+                    result = false;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            result = false;
+        }
+        return result;
+    }
+
+
+    /**
+     * 检查某个权限
+     *
+     * @param act         Activity对象
+     * @param permission  许可
+     * @param requestCode 请求代码
+     * @return boolean 返回true表示已启用该权限，返回false表示未启用该权限
+     */
+    public static boolean checkPermission(Activity act, String permission, int requestCode)
+    {
+        return checkPermission(act, new String[]{permission}, requestCode);
+    }
+
+
+    /**
+     * 检查多个权限
+     *
+     * @param act         Activity对象
+     * @param permissions 权限
+     * @param requestCode 请求代码
+     * @return boolean 返回true表示已完全启用权限，返回false表示未完全启用权限
+     */
+    @SuppressWarnings("all")
+    public static boolean checkPermission(Activity act, String[] permissions, int requestCode)
+    {
+        boolean result = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            int check = PackageManager.PERMISSION_GRANTED;
+            //通过权限数组检查是否都开启了这些权限
+            for (String permission : permissions)
+            {
+                check = ContextCompat.checkSelfPermission(act, permission);
+                if (check != PackageManager.PERMISSION_GRANTED)
+                {
+                    //有个权限没有开启，就跳出循环
+                    break;
+                }
+            }
+            if (check != PackageManager.PERMISSION_GRANTED)
+            {
+                //未开启该权限，则请求系统弹窗，好让用户选择是否立即开启权限
+                ActivityCompat.requestPermissions(act, permissions, requestCode);
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 显示消息
+     *
+     * @param message 消息
+     */
+    private void toastShow(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
